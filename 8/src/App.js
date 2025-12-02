483038f0df97d73ee203ceaaa3439009309e7cda
Irakli00 <iraklitsurtsumia@gmail.com>
2025-12-02 18:48:44 +0400

    8

---
+++ 8/src/App.js
@@ -0,0 +1,16 @@
+import me from "./me.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={me} className="App-logo" alt="logo" />
+
+        <h1>lorem ipusm</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
